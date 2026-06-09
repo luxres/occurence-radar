3f35a3884ec6d18be5d62aac3fda43b031e421a2
lux-maker <socpopcap@proton.me>
2026-06-09 17:19:22 +0200

    added csrf trusted origins as env variable

--- occurrence_radar/occurrence_radar/settings.py
+++ occurrence_radar/occurrence_radar/settings.py
@@ -28,6 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "").split(",")
 
+CSRF_TRUSTED_ORIGINS = os.getenv("DJANGO_CSRF_TRUSTED_ORIGINS", "").split(",")
 # Application definition
 
 INSTALLED_APPS = [
