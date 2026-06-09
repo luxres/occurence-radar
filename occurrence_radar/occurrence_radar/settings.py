72998e5c7eaf58018fc21183e18c9ce97ee64722
lux-maker <socpopcap@proton.me>
2026-06-09 17:32:30 +0200

    minor fixes for prototype deployment

--- occurrence_radar/occurrence_radar/settings.py
+++ occurrence_radar/occurrence_radar/settings.py
@@ -28,7 +28,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "").split(",")
 
-CSRF_TRUSTED_ORIGINS = os.getenv("DJANGO_CSRF_TRUSTED_ORIGINS", "").split(",")
+if os.getenv("DJANGO_CSRF_TRUSTED_ORIGINS"):
+    CSRF_TRUSTED_ORIGINS = os.getenv("DJANGO_CSRF_TRUSTED_ORIGINS", "").split(",")
+    
 # Application definition
 
 INSTALLED_APPS = [
